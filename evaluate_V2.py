fc3f281a53424cf6333656b12d0b190254ead542
Hee Hwang (Peter) <heehcs@gmail.com>
2021-05-11 20:57:43 -0400

    add local only objs

--- evaluate_V2.py
+++ evaluate_V2.py
@@ -1,3 +1,5 @@
+from tqdm import tqdm
+
 import argparse
 from datetime import datetime
 import numpy as np
@@ -37,16 +39,11 @@ import data_sdf_h5_queue # as data
 import create_file_lst
 
 # PC  : (218,3)
-# PC_GT: (2560,3)
+# PC_GT: (25OP0,3)
 
     
 
 OBJ_NO = 0
-
-
-
-
-
 
 
 lst_dir, cats, all_cats, raw_dirs = create_file_lst.get_all_info()
@@ -110,9 +107,9 @@ else:
 
 # two_stream = False
 # two_stream = True
-# mode = 'local_features_only'
+mode = 'local_features_only'
 # mode = 'global_features_only'
-mode = 'global_and_local_features'
+# mode = 'global_and_local_features'
 
 
 TEST_LISTINFO = []
@@ -141,11 +138,14 @@ info = {'rendered_dir': raw_dirs["renderedh5_dir"],
             'sdf_dir': raw_dirs['sdf_dir']}
 print(info)
 
+
+
+
 with torch.no_grad():
     net = sdfnet()
     # Here we would like to load a pre trained model
-    model_epoch = 99
-    # model_epoch = 75
+    #model_epoch = 99
+    model_epoch = 75
     net.load_state_dict(torch.load('models/sdfmodel_{}_{}.torch'.format(mode, model_epoch), map_location='cpu'))
     net.eval()
 
@@ -154,180 +154,53 @@ with torch.no_grad():
     # Use fetch to get random, use get_batch for the same everytime
 
     # obj_nm = 953a6c4d742f1e44d1dcc55e36186e4e, viewid=2
-    batch_data = TEST_DATASET.get_batch(650)
-    # Generate grid
-    N = 257
-    dist = 1
-    max_dimensions = np.array([dist, dist, dist])
-    min_dimensions = np.array([-dist, -dist, -dist])
-    bounding_box_dimensions = max_dimensions - min_dimensions
-    grid_spacing = max(bounding_box_dimensions)/N
-    X, Y, Z = np.meshgrid(list(np.arange(min_dimensions[0], max_dimensions[0], grid_spacing)),
-    list(np.arange(min_dimensions[1], max_dimensions[1], grid_spacing)),
-    list(np.arange(min_dimensions[2], max_dimensions[2], grid_spacing)))
-    X = X.reshape(-1)
-    Y = Y.reshape(-1)
-    Z = Z.reshape(-1)
-    points = np.array([X, Y, Z])
 
-    print('Load a sample image...')
-    # Demo Image: comment out below five lines unless you use the specified image
-    img_file = "./03001627_953a6c4d742f1e44d1dcc55e36186e4e_02.png"
-    #img_file = "03001627_d72f27e4240bd7d0283b00891f680579_00.png"
-    img_arr = cv2.imread(img_file, cv2.IMREAD_UNCHANGED).astype(np.uint8)[:, :, :3]
-    batch_img = np.asarray([img_arr.astype(np.float32) / 255.])
-    #batch_data = {}
-    batch_data['img'] = batch_img
-    print(batch_data.keys())
+    batch_no_vec = [100,201,300,400,500,550,1150]
+    for i in tqdm(range(len(batch_no_vec))):
+        batch_data = TEST_DATASET.get_batch(batch_no_vec[i])
+        # Generate grid
+        N = 257
+        dist = 1
+        max_dimensions = np.array([dist, dist, dist])
+        min_dimensions = np.array([-dist, -dist, -dist])
+        bounding_box_dimensions = max_dimensions - min_dimensions
+        grid_spacing = max(bounding_box_dimensions)/N
+        X, Y, Z = np.meshgrid(list(np.arange(min_dimensions[0], max_dimensions[0], grid_spacing)),
+                              list(np.arange(min_dimensions[1], max_dimensions[1], grid_spacing)),
+                              list(np.arange(min_dimensions[2], max_dimensions[2], grid_spacing)))
+        X = X.reshape(-1)
+        Y = Y.reshape(-1)
+        Z = Z.reshape(-1)
+        points = np.array([X, Y, Z])
+        print('Load a sample image...')
+        # Prediction & obj generation
+        print('Predict and generate .obj file...', end=' ')
+        image = torch.from_numpy(batch_data['img']).permute(0, 3, 1, 2)[OBJ_NO]
+        points = torch.from_numpy(points.astype('Float32')).permute(1,0)
+        trans_mat = torch.from_numpy(batch_data['trans_mat'])[OBJ_NO]
 
-    # 'pc', 'sdf_pt', 'sdf_pt_rot', 'sdf_val', 'norm_params', 'sdf_params',
-    # 'img', 'trans_mat', 'cat_id', 'obj_nm', 'view_id'
-    # print(batch_data['obj_nm'])
-    # print(batch_data['view_id'])
+        # ours_sdf = net(image.unsqueeze(0), points.unsqueeze(0))
 
-    print(batch_data['trans_mat'])
+        max_num_points = 3000000
+        num_chunks = int(np.ceil(points.shape[0]/max_num_points))
+        points_chunks = torch.chunk(points, num_chunks, dim=0)
 
+        pred_sdf = []
+        for c in tqdm(range(num_chunks)):
+            pred_sdf_chunk = net(image.unsqueeze(0), points_chunks[c].unsqueeze(0), trans_mat.unsqueeze(0), mode=mode)
+            pred_sdf.append(pred_sdf_chunk)
+        pred_sdf = torch.cat(pred_sdf, dim=1)
 
-    
-
-
-    
-
-    # Prediction & obj generation
-    print('Predict and generate .obj file...', end=' ')
-    image = torch.from_numpy(batch_data['img']).permute(0, 3, 1, 2)[OBJ_NO]
-    points = torch.from_numpy(points.astype('Float32')).permute(1,0)
-    trans_mat = torch.from_numpy(batch_data['trans_mat'])[OBJ_NO]
-
-    # ours_sdf = net(image.unsqueeze(0), points.unsqueeze(0))
-
-    max_num_points = 300000
-    num_chunks = int(np.ceil(points.shape[0]/max_num_points))
-    points_chunks = torch.chunk(points, num_chunks, dim=0)
-
-    pred_sdf = []
-    for c in range(num_chunks):
-        pred_sdf_chunk = net(image.unsqueeze(0), points_chunks[c].unsqueeze(0), trans_mat.unsqueeze(0), mode=mode)
-        pred_sdf.append(pred_sdf_chunk)
-    pred_sdf = torch.cat(pred_sdf, dim=1)
-
-    ours_sdf = pred_sdf
-
-    np_sdf = ours_sdf.numpy()
-    IF = ours_sdf.reshape(N,N,N)
-    IF = IF.permute(1,0,2)
-    verts_ours, simplices_ours = mcubes.marching_cubes(np.asarray(IF), 0)
-    mcubes.export_obj(verts_ours, simplices_ours, "obj/chair_ours.obj")
-    print('done.')
-    
-    # GT (Already Normalized)
-    print('Collect GT surface samples...', end=' ')
-    #obj_file_gt    = '../ssd1/datasets/ShapeNet/mesh/03001627/953a6c4d742f1e44d1dcc55e36186e4e/isosurf.obj'
-    obj_file_gt    = 'isosurf.obj'
-    mesh_gt        = trimesh.load_mesh(obj_file_gt, process=False)
-    pc_gt_surf, _  = trimesh.sample.sample_surface(mesh_gt, FLAGS.num_sample_points)
-    choice_gt      = np.random.randint(pc_gt_surf.shape[0], size=FLAGS.num_sample_points)
-    PC_GT          = pc_gt_surf[choice_gt, ...]
-    print('done.')
-    # OURS
-    print('Collect OURS surface samples...', end=' ')
-    obj_file_ours                     = 'obj/chair_ours.obj'
-    obj_file_ours_norm, centroid, m   = get_normalize_mesh(obj_file_ours, 'obj/chair_ours_norm.obj')
-    with open(obj_file_ours_norm, 'r', encoding='utf8') as f_ours:
-        obj_data_ours = f_ours.read()
-        pc_ours_surf,_                = obj_data_to_mesh3d(obj_data_ours)
-    #mesh_ours                         = trimesh.load_mesh(obj_file_ours_norm, process=False)
-    #pc_ours_surf, _                   = trimesh.sample.sample_surface(mesh_ours, FLAGS.num_sample_points)
-    choice_ours                       = np.random.randint(pc_ours_surf.shape[0], size=FLAGS.num_sample_points)
-    PC_OURS                           = pc_ours_surf[choice_ours, ...]
-    print('done.')
-    
-    # THEIRS
-    print('Collect OURS surface samples...', end=' ')
-#    obj_file_theirs                   = '../DISN_xar/demo/chair_theirs.obj'
-    obj_file_theirs                   = './chair_theirs.obj'
-    #obj_file_theirs_norm, centroid, m = get_normalize_mesh(obj_file_theirs, 'obj/')
-    obj_file_theirs_norm, centroid, m = get_normalize_mesh(obj_file_theirs, 'obj/chair_theirs_norm.obj')
-    with open(obj_file_theirs_norm, 'r', encoding='utf8') as f_theirs:
-        obj_data_theirs = f_theirs.read()
-        pc_theirs_surf,_                = obj_data_to_mesh3d(obj_data_theirs)
-    #mesh_theirs                       = trimesh.load_mesh(obj_file_theirs_norm, process=False)
-    #pc_theirs_surf, _                 = trimesh.sample.sample_surface(mesh_theirs, FLAGS.num_sample_points)
-    choice_theirs                     = np.random.randint(pc_theirs_surf.shape[0], size=FLAGS.num_sample_points)
-    PC_THEIRS                         = pc_theirs_surf[choice_theirs, ...]
-    PC_THEIRS[:, [0,2]]               = PC_THEIRS[:, [2,0]]
-    print('done.')
-    
-    print('Ground Truth')
-    print(PC_GT[:10])
-    print('Ours')
-    print(PC_OURS[:10])
-    print('Theirs')
-    print(PC_THEIRS[:10])
-
-    print('--------------------------------------------------------------------------------')
-    print('FSCORE')
-    SIDE_LEN = 1.4
-    ratios = [0.01, 0.02, 0.05, 0.10, 0.20]
-    print('Threshold(%)', end='\t')
-    for r in ratios:
-        pr = 100*r
-        print('%3.1f' % pr, end='\t')
-    print()
-    print('--------------------------------------------------------------------------------')        
-    print('OURS        ', end='\t')
-    for r in ratios:
-        print('%5.3f' % FSCORE(PC_OURS  , PC_GT, SIDE_LEN * r), end='\t')
-    print()
-    print('THEIRS      ', end='\t')    
-    for r in ratios:
-        print('%5.3f' % FSCORE(PC_THEIRS, PC_GT, SIDE_LEN * r), end='\t')
-    print('\n--------------------------------------------------------------------------------')
-    #ours
-    print('Our Distances:')
-    cd   = CD(PC_OURS, PC_GT)
-    print('Chamfer Distance      : %f' % cd)
-    emd  = EMD(PC_OURS, PC_GT)
-    print('Earth Mover\'s Distance: %f' % emd)
-
-    print('--------------------------------------------------------------------------------')
-    #Theirs
-    print('Their Distances:')
-    cd   = CD(PC_THEIRS, PC_GT)
-    print('Chamfer Distance      : %f' % cd)
-    emd  = EMD(PC_THEIRS, PC_GT)
-    print('Earth Mover\'s Distance: %f' % emd)
-
-    print('--------------------------------------------------------------------------------')
-    # Renderings
-    with open(obj_file_gt, 'r', encoding='utf8') as f_gt:
-        obj_data_gt = f_gt.read()
-        verts_gt    , simplices_gt     = obj_data_to_mesh3d(obj_data_gt)
-    print('GT shape')
-    print(verts_gt.shape)
-    print(simplices_gt.shape)        
-        
-    with open(obj_file_ours_norm, 'r', encoding='utf8') as f_ours:
-        obj_data_ours = f_ours.read()
-        verts_ours  , simplices_ours   = obj_data_to_mesh3d(obj_data_ours)
-    print('OURS shape')
-    print(verts_ours.shape)
-    print(simplices_ours.shape)
-        
-    with open(obj_file_theirs_norm, 'r', encoding='utf8') as f_theirs:
-        obj_data_theirs = f_theirs.read()
-        verts_theirs, simplices_theirs = obj_data_to_mesh3d(obj_data_theirs)
-    print('THEIRS shape')
-    print(verts_theirs.shape)
-    print(simplices_theirs.shape)
-    verts_theirs[:, [0,2]] = verts_theirs[:, [2,0]]
-
-    
-    HTML_rendering('GT'    , verts_gt    , simplices_gt    )
-    HTML_rendering('OURS'  , verts_ours  , simplices_ours  )
-    HTML_rendering('THEIRS', verts_theirs, simplices_theirs)
-    print('done.')
-
+        ours_sdf = pred_sdf
+        np_sdf = ours_sdf.numpy()
+        IF = ours_sdf.reshape(N,N,N)
+        IF = IF.permute(1,0,2)
+        verts_ours, simplices_ours = mcubes.marching_cubes(np.asarray(IF), 0)
+        mcubes.export_obj(verts_ours, simplices_ours, "obj/localonly/chair_ours_" + str(batch_no_vec[i]).zfill(4) + ".obj")
+        #mcubes.export_obj(verts_ours, simplices_ours, "obj/local_only/chair_ours_0100.obj")
+        print('done.')
+        # THEIRS
+        print('Collect THEIRS surface samples...', end=' ')
     
     TEST_DATASET.shutdown()
 
